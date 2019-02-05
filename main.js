b64c56feae03974a1f9d99155dc74feb8b1ab511
Jason <jmido8@yahoo.com>
2019-02-05 17:45:22 +0800

    Add flexbox layout and navbar

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function controlNav(){
+
+//selects hamburger icon
+let mainNav = document.getElementById('js-hamburger');
+//selects navbar
+let toggleNav = document.getElementById('js-navbar-toggle');
+
+//toggles hamburger navbar open/close
+toggleNav.addEventListener('click', function () {
     
-}
+    mainNav.classList.toggle('active');
+
+});
